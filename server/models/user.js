76be7dd06b8c3064ffc10e019927314c1af629ba
bpahuja <bpahuja_be16@thapar.edu>
2019-02-17 23:34:36 +0530

    Setup JWT

--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,47 @@
 var mongoose = require('mongoose');
 const validator = require('validator');
-var User = mongoose.model('User',{
-    email:{
-        type:String,
-        require:true,
-        trim:true,
-        minlength:1,
-        unique:true,
-        validate:{
-          validator: validator.isEmail,
-            message:"{VALUE} not a valid email"
+const jwt = require('jsonwebtoken');
+var UserSchema = new mongoose.Schema(
+    {
+        email:{
+            type:String,
+            require:true,
+            trim:true,
+            minlength:1,
+            unique:true,
+            validate:{
+                validator: validator.isEmail,
+                message:"{VALUE} not a valid email"
+            },
         },
-    },
-    password:{
-        type: String,
-        require: true,
-        minLength: 6,
-    },
-    tokens: [{
-        access:{
+        password:{
             type: String,
             require: true,
+            minLength: 6,
         },
-        token:{
-            type: String,
-            require: true,
-        }
-    }]
-});
+        tokens: [{
+            access:{
+                type: String,
+                require: true,
+            },
+            token:{
+                type: String,
+                require: true,
+            }
+        }]
+    }
+);
+
+UserSchema.methods.generateAuthToken = function(){
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  user.tokens.push({access, token});
+  return user.save().then(()=>{
+      return token;
+  });
+};
+
+var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
