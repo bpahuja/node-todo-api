3ae0ee5cd20f29877fd08c2786cdb60f8359953d
bpahuja <bpahuja_be16@thapar.edu>
2019-02-19 23:53:01 +0530

    Setup JWT_SECRET

--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ var UserSchema = new mongoose.Schema(
 UserSchema.methods.generateAuthToken = function(){
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET || 'abc123').toString();
   user.tokens.push({access, token});
   return user.save().then(()=>{
       return token;
@@ -57,7 +57,7 @@ UserSchema.statics.findByToken = function(token){
   var User = this;
   var decoded;
   try{
-    decoded = jwt.verify(token,'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET || 'abc123');
   } catch(e){
     return Promise.reject();
   }
